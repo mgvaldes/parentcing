dd10f8c965fff88ed5a89481edc4e7e9125c4509
Maria Gabriela Valdes Graterol <gaby@Maria-Gabriela-Valdes-Graterols-MacBook-Pro.local>
2011-11-06 20:32:15 -0430

    Cambios en variables privadas a publicas en PCPropetyType

--- src/com/ing3nia/parentalcontrol/models/utils/PCPropertyType.java
+++ src/com/ing3nia/parentalcontrol/models/utils/PCPropertyType.java
@@ -1,23 +1,23 @@
 package com.ing3nia.parentalcontrol.models.utils;
 
 public class PCPropertyType {
-	private static int LOW_BATTERY_CHECK = 0;
+	public static int LOW_BATTERY_CHECK = 0;
 	
-	private static int DEAD_BATTERY_CHECK = 1;
+	public static int DEAD_BATTERY_CHECK = 1;
 	
-	private static int GPS_CHECK = 2;
+	public static int GPS_CHECK = 2;
 	
-	private static int UPDATE_SYNC_INTERVAL = 3;
+	public static int UPDATE_SYNC_INTERVAL = 3;
 	
-	private static int INTERNAL_MOD_SYNC_INTERVAL = 4;
+	public static int INTERNAL_MOD_SYNC_INTERVAL = 4;
 	
-	private static int ALERT_DELIVERY_INTERVAL = 5;
+	public static int ALERT_DELIVERY_INTERVAL = 5;
 	
-	private static int ACTIVITY_TRACK_INTERVAL = 6;
+	public static int ACTIVITY_TRACK_INTERVAL = 6;
 	
-	private static int ROUTE_CHECK_INTERVAL = 7;
+	public static int ROUTE_CHECK_INTERVAL = 7;
 	
-	private static int SPEED_LIMIT = 8;
+	public static int SPEED_LIMIT = 8;
 	
 	//TODO Agregar resto de properties faltantes.
 }
