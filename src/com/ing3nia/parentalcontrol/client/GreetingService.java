c561be157729c4cb163a7900e8d7943f94edb4fa
Maria Gabriela Valdes Graterol <gaby@Maria-Gabriela-Valdes-Graterols-MacBook-Pro.local>
2011-11-13 18:56:07 -0430

    quitando cambios en clases de gwt

--- src/com/ing3nia/parentalcontrol/client/GreetingService.java
+++ src/com/ing3nia/parentalcontrol/client/GreetingService.java
@@ -1,16 +1,12 @@
 package com.ing3nia.parentalcontrol.client;
 
-//import com.google.gwt.user.client.rpc.RemoteService;
-//import com.google.gwt.user.client.rpc.RemoteServiceRelativePath;
+import com.google.gwt.user.client.rpc.RemoteService;
+import com.google.gwt.user.client.rpc.RemoteServiceRelativePath;
 
 /**
  * The client side stub for the RPC service.
  */
-//@RemoteServiceRelativePath("greet")
-//public interface GreetingService extends RemoteService {
-//	String greetServer(String name) throws IllegalArgumentException;
-//}
-
-public interface GreetingService {
-	//String greetServer(String name) throws IllegalArgumentException;
+@RemoteServiceRelativePath("greet")
+public interface GreetingService extends RemoteService {
+	String greetServer(String name) throws IllegalArgumentException;
 }
