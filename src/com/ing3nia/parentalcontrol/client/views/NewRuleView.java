7175dd84b7849899ec5835bdff4378f36fd8794e
Maria Gabriela Valdes <mgvaldesgraterol@gmail.com>
2011-11-14 22:31:48 -0430

    Changes in NewRuleView. It is not ready yet.

--- src/com/ing3nia/parentalcontrol/client/views/NewRuleView.java
+++ src/com/ing3nia/parentalcontrol/client/views/NewRuleView.java
@@ -1,8 +1,13 @@
 package com.ing3nia.parentalcontrol.client.views;
 
+import java.util.List;
+
+import com.google.gwt.user.client.ui.FlexTable;
 import com.google.gwt.user.client.ui.HTMLPanel;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.ListBox;
+import com.google.gwt.user.client.ui.TextBox;
+import com.google.gwt.user.datepicker.client.DatePicker;
 
 public class NewRuleView {
 	/**
@@ -33,6 +38,72 @@ public class NewRuleView {
 	private ListBox ruleTypeListBox;
 	
 	/**
-	 * 
+	 * Disabled functionalities list box.
 	 */
+	private ListBox disabledFunctionalitiesListBox;
+	
+	/**
+	 * Disabled functionalities table.
+	 */
+	private FlexTable disabledFunctionalitiesTable;
+	
+	/**
+	 * Disabled functionalities list.
+	 */
+	private List<String> disabledFunctionalities;
+	
+	/**
+	 * From date label.
+	 */
+	private Label fromDateLabel;
+	
+	/**
+	 * From date text box.
+	 */
+	private TextBox fromDateTextBox;
+	
+	/**
+	 * From date picker.
+	 */
+	private DatePicker fromDatePicker;
+	
+	/**
+	 * To date label.
+	 */
+	private Label toDateLabel;
+	
+	/**
+	 * To date text box.
+	 */
+	private TextBox toDateTextBox;
+	
+	/**
+	 * To date picker.
+	 */
+	private DatePicker toDatePicker;
+	
+	/**
+	 * From time lable.
+	 */
+	private Label fromTimeLabel;
+	
+	/**
+	 * Hour text box.
+	 */
+	private TextBox hourTextBox;
+	
+	/**
+	 * Time separator ':'.
+	 */
+	private Label timeSeparator;
+	
+	/**
+	 * Minute text box.
+	 */
+	private TextBox minuteTextBox;
+	
+	/**
+	 * Seconds text box.
+	 */
+	private TextBox secondsTextBox;
 }
